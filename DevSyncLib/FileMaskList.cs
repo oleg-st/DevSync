d907f3b33d5fe0c7913ada7e696f2a47c4c37fd3
Oleg Stepanischev <Oleg.Stepanischev@tatar.ru>
2019-12-09 18:14:40 +0300

    Excludes are like gitignore now and are much faster

--- DevSyncLib/FileMaskList.cs
+++ DevSyncLib/FileMaskList.cs
@@ -1,87 +1,242 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
 
 namespace DevSyncLib
 {
     public class FileMaskList
     {
-        private Regex _regex;
-        private readonly List<string> _masks = new List<string>();
+        private readonly List<Mask> _masks = new List<Mask>();
+        private readonly List<Mask> _negativeMasks = new List<Mask>();
 
-        protected StringBuilder MaskToRegex(string mask)
+        private class Mask
         {
-            var re = new StringBuilder(Regex.Escape(FsEntry.NormalizePath(mask)))
-                // *
-                .Replace("\\*", "[^/]*")
-                // ?
-                .Replace("\\?", "[^/]");
-
-            // slash in the beginning -> begin of string
-            re.Insert(0, re[0] == '/' ? '^' : '/');
-
-            // slash after mask
-            if (re[re.Length - 1] != '/')
+            public readonly bool HasSlash;
+            public readonly string Value;
+            public Mask(string value)
             {
-                re.Append('/');
-            }
+                HasSlash = value.Contains('/');
 
-            return re;
+                // remove first /
+                if (value.StartsWith('/'))
+                {
+                    value = value.Substring(1);
+                }
+
+                Value = value;
+            }
         }
 
-        public List<string> GetList()
+        public void Clear()
         {
-            return _masks;
+            _masks.Clear();
+            _negativeMasks.Clear();
         }
 
         public bool SetList(List<string> list)
         {
-            _masks.Clear();
-            var combinedRegex = new StringBuilder();
-            foreach (var mask in list)
+            Clear();
+            foreach (var item in list)
             {
-                var trimmedMask = mask.Trim();
-                if (!string.IsNullOrEmpty(trimmedMask))
+                var maskValue = item.TrimStart();
+                if (!string.IsNullOrWhiteSpace(maskValue) && !maskValue.StartsWith('#'))
                 {
-                    var re = MaskToRegex(trimmedMask);
-                    if (combinedRegex.Length > 0)
+                    bool negative;
+                    if (maskValue.StartsWith('!'))
                     {
-                        combinedRegex.Append('|');
+                        negative = true;
+                        maskValue = maskValue.Substring(1);
+                    }
+                    else
+                    {
+                        negative = false;
                     }
 
-                    combinedRegex.Append(re);
+                    var mask = new Mask(maskValue);
+                    if (negative)
+                    {
+                        _negativeMasks.Add(mask);
+                    }
+                    else
+                    {
+                        _masks.Add(mask);
+                    }
                 }
             }
 
-            try
+            return true;
+        }
+
+        private static unsafe bool MatchMask(char* textStart, char* textEnd,  Mask mask)
+        {
+            if (mask.HasSlash)
             {
-                _regex = combinedRegex.Length == 0 ? null : new Regex(combinedRegex.ToString(), RegexOptions.Compiled);
-                _masks.AddRange(list);
-                return true;
+                return MatchTextMask(textStart, textEnd, mask);
             }
-            catch (Exception)
+
+            // match every basename in text
+            var start = textStart;
+            for (var p = textStart; p < textEnd; p++)
             {
-                _regex = null;
-                return false;
+                if (*p == '/')
+                {
+                    if (start < p && MatchTextMask(start, p, mask))
+                    {
+                        return true;
+                    }
+
+                    start = p + 1;
+                }
             }
+
+            return start < textEnd && MatchTextMask(start, textEnd, mask);
+        }
+
+        /*
+         * https://www.codeproject.com/Articles/5163931/Fast-String-Matching-with-Wildcards-Globs-and-Giti
+         *
+         * Modified
+         */
+        private static unsafe bool MatchTextMask(char *textStart, char *textEnd, Mask mask)
+        {
+            char *text1Backup = null;
+            char* glob1Backup = null;
+
+            char *text2Backup = null;
+            char* glob2Backup = null;
+
+            fixed (char* globStart = mask.Value)
+            {
+                var globPointer = globStart;
+                var globEnd = globPointer + mask.Value.Length;
+                var textPointer = textStart;
+
+                while (textPointer < textEnd)
+                {
+                    if (globPointer < globEnd)
+                    {
+                        switch (*globPointer)
+                        {
+                            case '*':
+                                if (++globPointer < globEnd && *globPointer == '*')
+                                {
+                                    // trailing ** match everything after /
+                                    if (++globPointer >= globEnd)
+                                    {
+                                        return true;
+                                    }
+
+                                    // ** followed by a / match zero or more directories
+                                    if (*globPointer != '/')
+                                    {
+                                        return false;
+                                    }
+
+                                    // new **-loop, discard *-loop
+                                    text1Backup = null;
+                                    glob1Backup = null;
+                                    text2Backup = textPointer;
+                                    glob2Backup = ++globPointer;
+                                    continue;
+                                }
+
+                                // trailing * matches everything except /
+                                text1Backup = textPointer;
+                                glob1Backup = globPointer;
+                                continue;
+                            case '?':
+                                // match any character except /
+                                if (*textPointer == '/')
+                                {
+                                    break;
+                                }
+
+                                textPointer++;
+                                globPointer++;
+                                continue;
+                            default:
+                                if (*globPointer == '\\')
+                                {
+                                    // literal match \-escaped character
+                                    if (globPointer + 1 < globEnd)
+                                    {
+                                        globPointer++;
+                                    }
+                                }
+
+                                // match the current non-NUL character
+                                if (*globPointer != *textPointer)
+                                    break;
+
+                                textPointer++;
+                                globPointer++;
+                                continue;
+                        }
+                    }
+
+                    if (glob1Backup != null && *text1Backup != '/')
+                    {
+                        // *-loop: backtrack to the last * but do not jump over /
+                        textPointer = ++text1Backup;
+                        globPointer = glob1Backup;
+                        continue;
+                    }
+
+                    if (glob2Backup != null)
+                    {
+                        // **-loop: backtrack to the last **
+                        textPointer = ++text2Backup;
+                        globPointer = glob2Backup;
+                        continue;
+                    }
+
+                    // partial path match
+                    if (textPointer < textEnd && *textPointer == '/')
+                    {
+                        break;
+                    }
+
+                    return false;
+                }
+
+                // ignore trailing stars
+                while (globPointer < globEnd && *globPointer == '*')
+                {
+                    globPointer++;
+                }
+
+                // at end of text means success if nothing else is left to match
+                return globPointer >= globEnd;
+            }
+        }
+
+        private static unsafe bool MatchTextMasks(char* textStart, char* textEnd, IEnumerable<Mask> masks)
+        {
+            foreach (var mask in masks)
+            {
+                if (MatchMask(textStart, textEnd, mask))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public bool IsMatch(string path)
         {
-            if (string.IsNullOrEmpty(path) || _regex == null)
+            if (string.IsNullOrEmpty(path))
             {
                 return false;
             }
-            if (!path.StartsWith('/'))
+
+            unsafe
             {
-                path = "/" + path;
+                fixed (char* pathStart = path)
+                {
+                    var pathEnd = pathStart + path.Length;
+                    return !MatchTextMasks(pathStart, pathEnd, _negativeMasks) && 
+                           MatchTextMasks(pathStart, pathEnd, _masks);
+                }
             }
-            if (!path.EndsWith('/'))
-            {
-                path += "/";
-            }
-            return _regex.IsMatch(path);
         }
     }
 }
